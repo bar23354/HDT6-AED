b80cd1a2d95505c3fac02e095e45ca9046061b91
bar23354 <bar23354@uvg.edu.gt>
2024-02-28 07:55:57 -0600

    UML and base clases created

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos - seccion 40
+ * Nadissa Vela 23764 | Roberto Barreda 23354
+  */
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HDT6-AED
+Universidad del Valle de Guatemala
 Hoja de trabajo #6 - Algoritmos y Estructura de Datos
+Nadissa Vela 23764 | Roberto Barreda 23354
