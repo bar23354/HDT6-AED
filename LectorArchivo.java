4b28442321c951d9f32ba0d7d7a300eb4e1439ac
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2024-02-28 08:26:52 -0600

    Clase para que se pueda leer el archivo de cartas

--- LectorArchivo.java
+++ LectorArchivo.java
@@ -1,3 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class LectorArchivo {
 
+    public HashMap<String, String> leerArchivo(String fileName){
+        HashMap<String, String> cartas = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String linea;
+            while((linea = br.readLine())!= null){
+                String[] carta = linea.split("\\|");
+                cartas.put(carta[0], carta[1]);
+            }
+            
+            System.out.println("Cartas cargadas al sistema!");
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return cartas;
+    }
 }
