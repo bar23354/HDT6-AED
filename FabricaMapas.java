ed3d401795518c66fca7f35f2997c54d200b10ba
bar23354 <bar23354@uvg.edu.gt>
2024-03-03 10:37:59 -0600

    Fabrica y utilidades (sinergia necesaria)

--- FabricaMapas.java
+++ FabricaMapas.java
@@ -1,3 +1,25 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos - seccion 40
+ * Nadissa Vela 23764 | Roberto Barreda 23354
+  */
+
+import java.util.HashMap;
+import java.util.*;
+
 public class FabricaMapas {
+
+    public static Map<String, Carta> crearMapa(String tipoMapa) {
+        switch (tipoMapa) {
+            case "HashMap":
+                return new HashMap<>();
+            case "TreeMap":
+                return new TreeMap<>();
+            case "LinkedHashMap":
+                return new LinkedHashMap<>();
+            default:
+                throw new IllegalArgumentException("Tipo de mapa no válido: " + tipoMapa);
+        }
+    }
 
 }
--- Utilidades.java
+++ Utilidades.java
@@ -1,3 +1,47 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos - seccion 40
+ * Nadissa Vela 23764 | Roberto Barreda 23354
+  */
+
+import java.util.Scanner;
+
 public class Utilidades {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    /** 
+     * @param mensaje
+     */
+    public static void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+    
+    /** 
+     * @return String
+     */
+    public static String leerEntrada() {
+        return scanner.nextLine();
+    }
+    
+    /** 
+     * @return int
+     */
+    public static int leerEntero() {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    /** 
+     * @param entrada
+     * @return boolean
+     */
+    public static boolean validarEntradaSiNo(String entrada) {
+        return entrada.equalsIgnoreCase("si") || entrada.equalsIgnoreCase("no");
+    }
+
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J"); //ANSI "escape code" de acuerdo con Google
+        System.out.flush();
+    }
 
 }
